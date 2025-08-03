69fa4563e57d55d55de98f3e91cc0111feb47f2e
gknachnani <gknachnani@yahoo.com>
2025-08-03 07:05:11 -0500

    code skelton have been updated for document comparison

--- README.md
+++ README.md
@@ -48,7 +48,7 @@ git push
 ### for cloning my repo use this command
 
 ```
-git clone https://github.com/sunnysavita10/document_portal.git
+git clone https://github.com/gknachnani/document_portal.git
 ```
 
 ### minimum requirement for this project
--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -5,7 +5,7 @@ logger=CustomLogger().get_logger(__file__)
 
 class DocumentPortalException(Exception):
     """Custom exception for Document Portal"""
-    #def __init__(self,error_message):
+    # #def __init__(self,error_message):
     def __init__(self,error_message,error_details):    
         #_,_,exc_tb=sys.exc_info()
         _,_,exc_tb=error_details.exc_info()
