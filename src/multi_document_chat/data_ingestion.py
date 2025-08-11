926f088fa825e57c2eb695abc730848a9ffae9f9
gknachnani <gknachnani@yahoo.com>
2025-08-10 21:56:26 -0500

    data ingestion code is ready for multiple docs

--- src/multi_document_chat/data_ingestion.py
+++ src/multi_document_chat/data_ingestion.py
@@ -26,9 +26,9 @@ class DocumentIngestor:
             # sessionized paths
             self.session_id = session_id or f"session_{datetime.now(timezone.utc).strftime('%Y%m_%H%M%S')}_{uuid.uuid4().hex[:8]}"
             self.session_temp_dir = self.temp_dir / self.session_id
-            self.session_id_faiss_dir = self.faiss_dir / self.session_id
+            self.session_faiss_dir = self.faiss_dir / self.session_id
             self.session_temp_dir.mkdir(parents=True, exist_ok=True)
-            self.session_id_faiss_dir.mkdir(parents=True, exist_ok=True)
+            self.session_faiss_dir.mkdir(parents=True, exist_ok=True)
             
             self.model_loader = ModelLoader()
             self.log.info(
@@ -37,7 +37,7 @@ class DocumentIngestor:
                 faiss_base=str(self.faiss_dir), 
                 session_id=self.session_id,
                 temp_path=str(self.session_temp_dir),
-                faiss_path=str(self.session_id_faiss_dir)
+                faiss_path=str(self.session_faiss_dir)
                 )
         except Exception as e:
             self.log.error("Failed to initialize DocumentIngestor",error=str(e))
@@ -90,7 +90,21 @@ class DocumentIngestor:
 
     def _create_retriever(self,documents):
         try:
-            pass
+            splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=300)
+            chunks = splitter.split_documents(documents)
+            self.log.info("Documents split into chunks", total_chunks=len(chunks),session_id=self.session_id)
+            
+            embeddings = self.model_loader.load_embeddings()
+            vectorstore = FAISS.from_documents(documents=chunks, embedding=embeddings)
+            
+            # save FAISS index under session folder
+            vectorstore.save_local(str(self.session_faiss_dir))
+            self.log.info("FAISS index saved to disk", path=str(self.session_faiss_dir), session_id=self.session_id)
+            
+            retriever = vectorstore.as_retriever(search_type = "similarity", search_kwargs={"k": 5})
+            self.log.info("FAISS retriever created and ready to use",session_id=self.session_id)
+            return retriever
+            
         except Exception as e:
             self.log.error("Failed to create retriever",error=str(e))
             raise DocumentPortalException("Retriever error in DocumentIngestor",sys) from e
