73e621f9ebf521d27459ab91ffa9e881f693a304
gknachnani <gknachnani@yahoo.com>
2025-08-13 04:43:05 -0500

    testing file is updated

---
+++ test_document_analyzer.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+from io import BytesIO
+from src.document_analyzer.data_ingestion import DocumentHandler
+from src.document_analyzer.data_analysis import DocumentAnalyzer
+
+    
+pdf_path=r"C:\\Users\\giris\document_portal\\data\\document_analysis\\sample.pdf"
+
+class DummyFile:
+    def __init__(self,file_path):
+        self.name = Path(file_path).name
+        self._file_path = file_path
+    def getbuffer(self):
+        return open(self._file_path, "rb").read()
+    
+
+def main():
+    try:
+        # ---------- STEP 1: DATA INGESTION ----------
+        print("Starting PDF ingestion...")
+        dummy_pdf = DummyFile(pdf_path)
+        
+        handler = DocumentHandler(session_id="test_ingestion_analysis")
+        saved_path = handler.save_pdf(dummy_pdf)
+        print(f"PDF saved at: {saved_path}")
+        
+        text_content = handler.read_pdf(saved_path)
+        print(f"Extracted text length: {len(text_content)} chars\n")
+        
+        
+        # ---------- STEP 2: DATA ANALYSIS ----------
+        print("Starting document analysis...")
+        analyzer = DocumentAnalyzer()
+        analysis_result = analyzer.analyze_document(text_content)
+        
+        # ---------- STEP 3: DISPLAY RESULTS ----------
+        print("\n=== METADATA ANALYSIS RESULT ===")
+        for key, value in analysis_result.items():
+            print(f"{key}: {value}")
+
+    except Exception as e:
+        print(f"Test Failed: {e}")
+        
+if __name__ == "__main__":
+    main()
---
+++ test_multi_document_chat.py
@@ -0,0 +1,48 @@
+## testoing for multidoc chat
+import sys
+from pathlib import Path
+from src.multi_document_chat.data_ingestion import DocumentIngestor
+from src.multi_document_chat.retrieval import ConversationalRAG
+
+def test_document_ingestion_and_rag():
+    try:
+        test_files = [
+            r"data\multi_doc_chat\market_analysis_report.docx",
+            r"data\multi_doc_chat\NIPS-2017-attention-is-all-you-need-Paper.pdf",
+            r"data\multi_doc_chat\sample.pdf",
+            r"data\multi_doc_chat\state_of_the_union.txt"
+        ]
+        
+        
+        uploaded_files =[]
+        for file_path in test_files:
+            if Path(file_path).exists():
+                # with open(file_path, "rb") as f:
+                #     uploaded_files.append(f)
+                uploaded_files.append(open(file_path, "rb"))
+            else:
+                print(f"File does not exist: {file_path}")
+                
+        if not uploaded_files:
+            print("No valid files to upload")
+            sys.exit(1)
+            
+        ingestor = DocumentIngestor()
+        retiever = ingestor.ingest_files(uploaded_files)
+        
+        for f in uploaded_files:
+            f.close()
+        
+        session_id = "test_multi_doc_chat"
+        rag = ConversationalRAG(retriever=retiever, session_id=session_id)
+        question = "What is attention is all you need paper about?"
+        
+        answer = rag.invoke(question)
+        print(f"\nQuestion: {question}\nAnswer: {answer}")
+        print("\nTest completed successfully!")
+    except Exception as e:
+        print(f"Test Failed: {e}")
+        sys.exit(1)
+        
+
+    
