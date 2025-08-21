7cd77d269e01df60b43328ac26bfa4eee1acde72
gknachnani <gknachnani@yahoo.com>
2025-08-21 05:27:34 -0500

    changes updated for logger

--- src/document_compare/document_comparator.py
+++ src/document_compare/document_comparator.py
@@ -4,6 +4,7 @@ import pandas as pd
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
 from utils.model_loader import ModelLoader
+from logger import GLOBAL_LOGGER as log
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from prompt.prompt_library import PROMPT_REGISTRY
@@ -14,14 +15,13 @@ from model.models import *
 class DocumentComparatorLLM:
     def __init__(self):
         load_dotenv()
-        self.log = CustomLogger().get_logger(__name__)
         self.loader = ModelLoader()
         self.llm =  self.loader.load_llm()
         self.parser = JsonOutputParser(pydantic_object=SummaryResponse)
         self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
         self.prompt = PROMPT_REGISTRY[PromptType.DOCUMENT_COMPARISION.value]
         self.chain = self.prompt | self.llm | self.parser
-        self.log.info("DocumentComparatorLLM initialized", model=self.llm)
+        log.info("DocumentComparatorLLM initialized", model=self.llm)
         
         
     
@@ -31,21 +31,21 @@ class DocumentComparatorLLM:
                 "combined_docs" : combined_docs,
                 "format_instruction" : self.parser.get_format_instructions()
             }
-            self.log.info("Invoking document comparision LLM chain")
+            log.info("Invoking document comparision LLM chain")
             response = self.chain.invoke(inputs)
-            self.log.info("Chain invoked successfully", response_preview=str(response)[:200])
+            log.info("Chain invoked successfully", response_preview=str(response)[:200])
             return self._format_response(response)  
 
             
         except Exception as e:
-            self.log.error("Error in compare_documents", error=str(e))
+            log.error("Error in compare_documents", error=str(e))
             raise DocumentPortalException("Error comparing documents", sys) from e
     
     def _format_response(self, response_parsed: list[dict]) -> pd.DataFrame:
         try:
             df = pd.DataFrame(response_parsed)
-            self.log.info("Response formatted into DataFrame", dataframe=df)
+            log.info("Response formatted into DataFrame", dataframe=df)
             return df
         except Exception as e:
-            self.log.error("Error formatting response into DataFrame", error=str(e))
+            log.error("Error formatting response into DataFrame", error=str(e))
             raise DocumentPortalException("Error formatting response", sys) from e
